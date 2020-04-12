0a1fbc6cba54cedd3c1ad3e526e7a5d9692e85b8
abdelghani benabderrahmane <ghanoumahbol@gmail.com>
2020-04-12 23:41:53 +0100

    [testdb.php] for testing database connection using openshift

---
+++ testdb.php
@@ -0,0 +1,23 @@
+<?php
+
+$dbhost = getenv("MYSQL_SERVICE_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_SERVICE_PORT");
+$dbuser = getenv("MYSQL_USER");
+$dbpwd = getenv("MYSQL_PASSWORD");
+$dbname = getenv("MYSQL_DATABASE");
+
+$con = mysqli_connect($dbhost.':'.$dbport,$dbuser,$dbpwd,$dbname);
+if(!$con){
+   echo "Could not connect to database";
+}else{
+    echo "Connection to database.<br>";
+}
+$sql = "show tables";
+$rs = mysqli_query($con,$sql);
+while ($row = mysqli_fetch_assoc($rs)){
+  echo '<pre>';
+  print_r($row);
+  echo '</pre>';
+}
+mysqli_close();
+?>
